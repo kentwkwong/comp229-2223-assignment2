3fabb9fcb803fbfacb18d28e2d2ab19c3d51d395
Kent Wong <kentwkwong@gmail.com>
2022-10-29 00:08:48 -0400

    Update connection string

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-    "URI": "mongodb://127.0.0.1/book_store"
+    //"URI": "mongodb://127.0.0.1/book_store"
+    "URI": "mongodb+srv://dbconnectuser:eyuyLGvI97LihnQi@mongodbserver.uq7npjq.mongodb.net/book_store?retryWrites=true&w=majority"
 }
